3c5c2a67b48861f1aed6ef54c75e991b609870ed
Matthias Urlichs <smurf@debian9.image-pipeline.noris.de>
2019-07-01 20:51:46 +0200

    Add "ring" mode

---
+++ calltest/mode/ring.py
@@ -0,0 +1,17 @@
+# DTMF: exchange calls, 
+
+import anyio
+
+from . import BaseOutWorker
+from . import wait_ringing
+
+import logging
+logger = logging.getLogger(__name__)
+
+class Worker(BaseOutWorker):
+    async def __call__(self):
+        async with self.out_call() as ocm:
+
+            await self.connect_out(ocm, handle_ringing=True)
+            await anyio.sleep(1)
+                
